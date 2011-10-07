04fb1d4c51b8ee1a78e24bf4aaaf09d6e4762d9b
Jason <thebizztech@me.com>
2011-10-07 15:21:51 -0500

    Index.php had a required parameter commented out to test the exceptions. Removed the comment so it is fully functional

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $result = $wpp
 			->set_paymentaction()
 			->set_creditcard(array("creditcardtype" => "Visa", "acct" => "4683075410516684", "expdate" => "102015", "cvv2" => "123"))
 			->set_name(array("first" => "Jason", "last" => "Michels"))
-			//->set_address(array("street" => "123", "city" => "Papillion", "state" => "NE", "zip" => "68046", "countrycode" => "US"))
+			->set_address(array("street" => "123", "city" => "Papillion", "state" => "NE", "zip" => "68046", "countrycode" => "US"))
 			->set_amt("10.25")
 			->execute();
 
