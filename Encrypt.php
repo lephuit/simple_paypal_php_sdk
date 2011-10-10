7150d6804feee53ebf17aba16c8b58f01d5f5f9f
Jason <thebizztech@me.com>
2011-10-10 16:47:20 -0500

    Added an Encrypt class that will be used to encode data before entering into database and it will have function to encode it also.

---
+++ Encrypt.php
@@ -0,0 +1,20 @@
+<?php
+
+class Encrypt{
+
+
+	function __construct() 
+	{
+	}
+
+	public function encode($string)
+	{
+	}
+
+	public function decode($string)
+	{
+	}
+	
+}
+
+/* End of Encrypt.php class */
